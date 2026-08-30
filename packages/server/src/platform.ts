b431379222c1f16c97429c26bacce1d25be8ace7
Myriad-Dreamin <camiyoru@gmail.com>
2026-08-30 09:55:24 -0700

    feat(packages): an Agent's definition publishes to a GitHub gist and installs from one
    (cherry picked from commit 64e3b04e3305e763c760dd827919728e62946cd5)
    (cherry picked from commit 48bc38bc54293929d1bb82586c6b2e7f69ffaf4f)

--- packages/server/src/platform.ts
+++ packages/server/src/platform.ts
@@ -61,6 +61,7 @@ import {
 } from "./runtime/session-manager.js";
 import {
   GlobalFetch,
+  HttpFetch,
   UpdateCheckService,
   VersionRoutes,
   UpdateCheck,
@@ -148,6 +149,9 @@ import { Schedules, Scheduling, SessionIndex, SessionOrigins } from "./mechanism
 import { Workflows } from "./mechanisms/workflows.js";
 import { WorkflowService } from "./workflows/service.js";
 import { WorkflowPrompt, WorkflowRoutes } from "./workflows/routes.js";
+import { AgentPackages } from "./mechanisms/packages.js";
+import { AgentPackageService } from "./packages/service.js";
+import { PackageRoutes } from "./packages/routes.js";
 import {
   ErrorLog,
   Errors,
@@ -444,7 +448,7 @@ export class CompanyModule {}
     LanguagesModule,
     LanguageRoutes,
   ],
-  exports: [Http, WebShell, UpdateCheck],
+  exports: [Http, WebShell, UpdateCheck, HttpFetch],
 })
 export class ApiModule {}
 
@@ -453,6 +457,12 @@ export class ApiModule {}
   exports: [Workflows],
 })
 export class WorkflowsModule {}
+
+@Module({
+  children: [AgentPackageService, PackageRoutes],
+  exports: [AgentPackages],
+})
+export class PackagesModule {}
 
 /** The root: provides nothing and requires nothing; it exists so the groups have a scope to see each other in. */
 @Module({
@@ -476,6 +486,7 @@ export class WorkflowsModule {}
     MachinesModule,
     TerminalRelay,
     WorkflowsModule,
+    PackagesModule,
     Startup,
   ],
 })
